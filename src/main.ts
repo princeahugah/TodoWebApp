c659417df9fdbac1b25e8bb799ce7deeb55a354d
Prince Ahugah <prince.ahugah@outlook.com>
2020-06-11 03:11:24 +0000

    removed axios

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import axios from 'axios';
 import VueMeta from 'vue-meta';
 
 Vue.use(VueMeta);
